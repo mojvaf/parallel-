e36e6b67c9202a04ab859ae03112257b16533d96
mojvaf <mvafa44@gmail.com>
2020-11-02 13:35:53 +0100

    applied output to html

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// html setup
+ var pupilsHTMLCollection = document.getElementsByClassName('pupil')
+ var pupilsArray = Array.from(pupilsHTMLCollection)
+ console.log('pupilsArray', pupilsArray)
+
+
+// input setup
 var input = {
     mouseX :{
         start:0,
@@ -11,19 +18,57 @@ var input = {
     }
 };
 
-// range
 input.mouseX.range = input.mouseX.end - input.mouseX.start;
 input.mouseY.range= input.mouseY.end - input.mouseY.start
 
+
+//output setup
+var output = {
+    x:{
+        start: -45,
+        end: 45,
+        current:0,
+    },
+    y:{
+        start: -45,
+        end: 45,
+        current:0,
+    },
+}
+
+output.x.range = output.x.end -output.x.start;
+output.y.range = output.y.end - output.y.start;
+
+
 var handelMouseMove = function(event){
+    // mouse x input
     input.mouseX.current = event.clientX;
     input.mouseX.fraction = (input.mouseX.current - input.mouseX.start)/input.mouseX.range
 
+    //mouse y input
     input.mouseY.current = event.clientY
     input.mouseY.fraction=(input.mouseY.current - input.mouseY.start)/input.mouseY.range
     
-    console.log('fraction X', input.mouseX.fraction)
-    console.log('fraction Y', input.mouseY.fraction)
+    // output x
+    output.x.current = output.x.end - (input.mouseX.fraction * output.x.range)
+    output.x.opposite = output.x.start + (input.mouseX.fraction * output.x.range)
+    // output y
+    output.y.current = output.y.end -(input.mouseY.fraction * output.y.range)
+    output.y.opposite = output.y.start + (input.mouseY.fraction * output.y.range)
+    // apply output to html
+    pupilsArray.forEach((pupil, i)=>{
+
+      if(i === 0){
+        pupil.style.transform = 'translate('+output.x.opposite+'px, '+output.y.opposite+'px)'
+      }else{
+        pupil.style.transform = 'translate('+output.x.current+'px, '+output.y.current+'px)'
+      }
+
+    })
+
+
+    console.log('output.x.current', output.x.current)
+    //console.log('fraction Y', input.mouseY.fraction)
 }
 
 
